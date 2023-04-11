ab61f4bba4a69f7bdebce41176938f574f321460
Hea in SONG <hesong@e1r11p1.clusters.42paris.fr>
2023-04-11 18:58:44 +0200

    ,,

---
+++ Ring-02/Exam-Rank-02/Level-03/str_capitalizer/2str.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+
+void	ft_capitalizer(char *str)
+{
+	int k = 0;
+	char tmp;
+	
+	while(str[k] != '\0')
+	{
+		if (k == 0) // the first char of the entire string
+		{
+			if (str[k] >= 'a' && str[k] <= 'z') // lower case -> upper case
+			{
+				tmp = str[k] - 32;
+				write(1, &tmp, 1);
+			}
+			else // otherwise all printout without changing
+				write(1, &str[k], 1);
+		}
+		else // if it's not first char of string(middle or end)
+		{
+			if (str[k] != ' ' && str[k] != '\t') // if it's not space or tab
+			{
+				if (str[k - 1] == ' ' || str[k - 1] == '\t') // if it's a first word of each string
+				{
+					if (str[k] >= 'a' && str[k] <= 'z') //lower case -> upper case
+					{
+						tmp = str[k] - 32;
+						write(1, &tmp, 1);
+					}
+					else
+						write(1, &str[k], 1);
+				}
+				else // if it's not the first word of each string(middle or end)
+				{
+					if (str[k] >= 'a' && str[k] <= 'z')
+						write(1, &str[k], 1);
+					else if (str[k] >= 'A' && str[k] <= 'Z')
+					{
+						tmp = str[k] + 32;
+						write(1, &tmp, 1);
+					}
+					else
+						write(1, &tmp, 1);
+				}
+			}
+			else // if it's space or tab
+				write(1, &str[k], 1);
+		}
+		k++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+
+	if (argc >= 2)
+	{
+		while (argc >= 1)
+		{
+			ft_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i += 1;
+			argc--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
