0efda20cae97f6baee66ce751e0791c708a6a90e
Hea in SONG <hesong@e1r4p1.clusters.42paris.fr>
2023-04-15 22:02:59 +0200

    alpha_mirror

--- Ring-02/Exam-Rank-02/Level-02/alpha_mirror/alpha_mirror.c
+++ Ring-02/Exam-Rank-02/Level-02/alpha_mirror/alpha_mirror.c
@@ -2,36 +2,26 @@
 
 int main(int argc, char **argv)
 {
-	int i;
-	char tmp;
-
-	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
+		char *str = argv[1];
+		int i = 0;
+		char tmp;
+
+		while(str[i] != '\0')
 		{
-			if (argv[1][i] >= 97 && argv[1][i] <= 109)
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				tmp = argv[1][i] + (25 - (2 *(argv[1][i] - 97)));
+				tmp = 122 - (str[i] - 97);
 				write(1, &tmp, 1);
 			}
-			else if (argv[1][i] >= 65 && argv[1][i] <= 77)
+			else if (str[i]>= 'A' && str[i] <= 'Z')
 			{
-				tmp = argv[1][i] + (25 - (2 * (argv[1][i] - 65)));
-				write(1, &tmp, 1);
-			}
-			else if (argv[1][i] >= 110 && argv[1][i] <= 122)
-			{
-				tmp = argv[1][i] - (25 - (2 * (122 - argv[1][i])));
-				write(1, &tmp, 1);
-			}
-			else if (argv[1][i] >= 78 && argv[1][i] <= 90)
-			{
-				tmp = argv[1][i] - (25 - (2 * (90 - argv[1][i])));
+				tmp = 90 - (str[i] - 65);
 				write(1, &tmp, 1);
 			}
 			else
-				write(1, &argv[1][i], 1);
+				write(1, &str[i], 1);
 			i++;
 		}
 	}
