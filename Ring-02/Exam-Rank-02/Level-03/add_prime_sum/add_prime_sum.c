f91eceec8f1bec70f35ce7e4b163ed6acddf57b7
Hea In Song <heain.song328@gmail.com>
2023-04-24 08:00:44 +0200

    add_prime_sum

--- Ring-02/Exam-Rank-02/Level-03/add_prime_sum/add_prime_sum.c
+++ Ring-02/Exam-Rank-02/Level-03/add_prime_sum/add_prime_sum.c
@@ -1,83 +1,64 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-void ft_putnbr(int num)
+void ft_putnbr(int nbr)
 {
 	char tmp;
 
-	if (num > 9)
-		ft_putnbr(num / 10);
-	tmp = (num % 10) + 48 ;
+	if (nbr > 9)
+		ft_putnbr(nbr / 10);
+	tmp = (nbr % 10) + 48;
 	write(1, &tmp, 1);
 }
+
+int is_prime(int p)
+{
+	int i = 2;
+
+	while (i <= (p / 2))
+	{
+		if (p % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int ft_atoi(char *str)
 {
 	int i = 0;
-	long int result = 0;
+	int result;
 
-	if (str[i] == '-')
+	while (str[i] != '\0')
 	{
-		write(1, "0\n", 2);
-		exit(0);
-	}
-	while(str[i] != '\0')
-	{
-		if (str[i] >= 48 && str[i] <= 59)
-		{
-			result *= 10;
-			result += (str[i] - 48);
-		}
+		if (str[i] >= '0' && str[i] <= '9')
+			result = (result * 10) + (str[i] - 48);
+		else
+			return(-1);
 		i++;
 	}
 	return (result);
 }
 
-int is_prime(int n)
-{
-	int j = 2;
-	while (j <= (n / 2))
-	{
-		if (n % j == 0)
-		{	
-			//break;
-			return (0);
-		}
-		j++;
-	}
-	return (1);
-}
-
 int main(int argc, char **argv)
 {
+	int nbr = ft_atoi(argv[1]);
 
-	if (argc == 2 && argv[1] != NULL)
+	if (argc == 2 && nbr > 0)
 	{
-		int end_nbr = ft_atoi(argv[1]);
-		if (end_nbr == 0 || end_nbr == 1)
+		int p = 2; // prime number starts from 2
+		int result = 0;
+
+		while (p <= nbr)
 		{
-			write(1, "0\n", 2);
-			return(0);
+			if (is_prime(p) == 1)
+				result += p;
+			p++;
 		}
-		else //positive nbr
-		{
-			int n = 2;
-			int sum_result = 0;
-			while(n <= end_nbr)
-			{
-				if (is_prime(n) == 1)
-				{
-					sum_result += n;
-				}
-				n++;
-			}
-			//printf("result = %d\n", sum_result);
-			ft_putnbr(sum_result);
-		}
+		ft_putnbr(result);
 	}
-	else if (argv[1] == NULL)
-		write(1, "0\n", 2);
-
-return (0);
-
+	else
+		write (1, "0", 1);
+	write(1, "\n", 1);
+	return (0);
 }
+
