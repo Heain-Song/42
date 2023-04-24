11a355239dd23605658869f135918b9fa5803838
Hea In Song <heain.song328@gmail.com>
2023-04-24 08:51:09 +0200

    expand_str

--- Ring-02/Exam-Rank-02/Level-03/expand_str/expand_str.c
+++ Ring-02/Exam-Rank-02/Level-03/expand_str/expand_str.c
@@ -1,28 +1,35 @@
 #include <unistd.h>
 
+int is_space(char c)
+{
+	if (c == ' ' || c == '\t')
+		return (1);
+	else
+		return (0);
+}
+
 int main(int argc, char **argv)
 {
-	int i = 0;
-
-	char *str = argv[1];
-	if (argc == 2)
+	if (argc == 2 && argv[1] != NULL)
 	{
-		while(str[i] == ' ' || str[i] == '\t')
+		int i = 0;
+		int flag = 0;
+		char *str = argv[1];
+
+		while (is_space(str[i]))
 			i++;
+		flag = i;
 		while (str[i] != '\0')
-		{		
-			if (str[i] == ' ' || str[i] == '\t') 
+		{
+			if (!is_space(str[i]))
 			{
-				if (str[i + 1] == '\0')
-					break;
-				if (str[i + 1] != ' ' && str[i + 1] != '\t')
+				if ((i - 1) > flag && is_space(str[i - 1]))
 					write(1, "   ", 3);
-			}
-			if (str[i] != ' ' && str[i] != '\t')
 				write(1, &str[i], 1);
+			}
 			i++;
 		}
 	}
-	write(1, "\n",1);
+	write(1, "\n", 1);
 	return (0);
 }
