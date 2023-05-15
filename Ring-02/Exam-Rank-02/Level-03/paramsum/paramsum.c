d13bf1056d7edf08e1ebd3a1acd32735b47ec9cc
Hea In Song <heain.song328@gmail.com>
2023-05-15 08:04:38 +0200

    paramsum

--- Ring-02/Exam-Rank-02/Level-03/paramsum/paramsum.c
+++ Ring-02/Exam-Rank-02/Level-03/paramsum/paramsum.c
@@ -21,3 +21,32 @@ int main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+/* RETRY(15.May.2023) - SUCCESS
+
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putnbr(int nbr)
+{
+	char tmp;
+
+	if (nbr > 9)
+		ft_putnbr(nbr / 10);
+	tmp = (nbr % 10) + 48;
+	write(1, &tmp, 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc >= 2)
+	{
+		ft_putnbr((argc - 1));
+	}
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+	return (0);
+}
+
+*/
