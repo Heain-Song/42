4589d0ae7eda9434e109ec309b3afc3fd209e55b
Hea In Song <heain.song328@gmail.com>
2023-05-15 20:29:30 +0200

    wdmatch

--- Ring-02/Exam-Rank-02/Level-02/wdmatch/wdmatch.c
+++ Ring-02/Exam-Rank-02/Level-02/wdmatch/wdmatch.c
@@ -11,37 +11,39 @@ int ft_strlen(char *str)
 
 int main(int argc, char **argv)
 {
-	int i = 0;
-	int j = 0;
-	int count = 0;
-	int start = 0;
-	int flag = 0;
-
-	char *s1 = argv[1];
-	char *s2 = argv[2];
-
-	while (s1[i] != '\0')
+	if (argc == 3)
 	{
-		j = start;
-		while(s2[j] != '\0')
+		int i = 0;
+		int j = 0;
+		int count = 0;
+		int start = 0;
+	
+		char *s1 = argv[1];
+		char *s2 = argv[2];
+
+		while (s1[i] != '\0')
 		{
-			if (s1[i] == s2[j])
+			j = start;
+			while(s2[j] != '\0')
 			{
-				count++;
-				start = j;
-				break;
+				if (s1[i] == s2[j])
+				{
+					count++;
+					start = j;
+					break;
+				}
+				j++;
 			}
-			j++;
-		}
-		i++;
-	}
-	if (count == ft_strlen(s1))
-	{
-		i = 0;
-		while(s1[i] != '\0')
-		{
-			write(1, &s1[i], 1);
 			i++;
+		}
+		if (count == ft_strlen(s1))
+		{
+			i = 0;
+			while(s1[i] != '\0')
+			{
+				write(1, &s1[i], 1);
+				i++;
+			}
 		}
 	}
 	write(1, "\n", 1);
