5a2716b6260f06bb4e35f6a95223f64df49cfe54
Hea In Song <heain.song328@gmail.com>
2023-05-15 07:35:38 +0200

    ft_strcpy practice

---
+++ Ring-02/Exam-Rank-02/Level-01/ft_strcpy/test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+	char *dest = s1;
+	char *src = s2;
+
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
+}
+
+//my main
+
+int main(void)
+{
+	char *s1 = "Hello"; //dest 
+	char *s2 = "Bonjour"; //src
+
+	printf("s1: %s\n", s1);
+	ft_strcpy(s1, s2);
+	printf("s1: %s\n", s1);
+	//printf("ft_strcpy: %s\n", ft_strcpy(s1, s2));
+	//printf("strcpy: %s\n", strcpy(s1, s2));
+	return(0);
+}
+
+// with another main, it works.
+
+/*int main(void)
+{
+	char str1[] = "Flavio";
+	char str2[] = "Gaucho";
+
+	printf("%s\n", str1);   // displaying what str1 is before ft_strcpy is used on it
+	ft_strcpy(str1, str2);  // calling out functtion ft_strcpy on the strings
+	printf("%s\n", str1);  // str1 should now become str2
+	return (0);
+}*/
