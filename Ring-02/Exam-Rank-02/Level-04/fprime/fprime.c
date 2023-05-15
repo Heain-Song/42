472a6160e8f78bc4d52143308bd6151a5d27f8aa
Hea In Song <heain.song328@gmail.com>
2023-05-15 08:55:49 +0200

    fprime

--- Ring-02/Exam-Rank-02/Level-04/fprime/fprime.c
+++ Ring-02/Exam-Rank-02/Level-04/fprime/fprime.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_prime(int nbr)
+{
+	if (nbr <= 1)
+		return (0);
+	int i = 2;
+	while(i < nbr)
+	{
+		if (nbr % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+
+}
 int main(int argc, char **argv)
 {
 	if (argc == 2)
@@ -8,13 +22,34 @@ int main(int argc, char **argv)
 		int nbr = atoi(argv[1]);
 
 		if (nbr == 1)
-			write(1, "1", 1);
+			printf("1");
+		
 		else
 		{
-
+			int i = 2;
+			while(i <= nbr)
+			{
+				if (is_prime(i) == 1)
+				{
+					if (nbr % i == 0)
+					{
+						printf("%d", i);
+						nbr = nbr / i;
+						if (nbr != 1)
+							printf("*");
+						i = 2;
+					}
+				}
+				i++;
+			}
 		}
 
 	}
-	write(1, "\n", 1);
+	printf("\n");
 	return (0);
 }
+
+/*
+SUCCESS, but why it does not work when the argv[1] = 804577?
+
+*/
